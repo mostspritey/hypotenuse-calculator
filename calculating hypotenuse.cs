03f17de2dc4b0c878c6c472ada4fc25eaf061fd6
mostspritey <145297398+mostspritey@users.noreply.github.com>
2023-09-18 21:11:41 +0100

    Add files via upload

---
+++ calculating hypotenuse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cats = 35;
+
+            Console.WriteLine("please enter the number of cats you gained: ");
+            int mc = (Console.ReadKey());
+
+
+            int tc = cats + mc;
+
+            Console.WriteLine("your total number of cats is: " + tc);
+
+            Console.ReadKey();
+        }
+    }
+}
